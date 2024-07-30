a51b7ce5fa2e8ce12c8392ffe00be4ca82b54325
TUTKU\tutku <tutku.ince@outlook.com>
2024-07-30 21:09:26 +0300

    food-catalogue-service initializer

---
+++ food-catalogue-service/src/main/java/com/incetutku/foodcatalogueservice/FoodCatalogueServiceApplication.java
@@ -0,0 +1,13 @@
+package com.incetutku.foodcatalogueservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodCatalogueServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodCatalogueServiceApplication.class, args);
+    }
+
+}
