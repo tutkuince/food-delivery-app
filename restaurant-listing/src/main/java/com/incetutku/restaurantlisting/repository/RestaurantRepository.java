3b75acc451d4b65a05f767de61c7ae45cfca723b
TUTKU\tutku <tutku.ince@outlook.com>
2024-07-30 18:03:26 +0300

    repo

---
+++ restaurant-listing/src/main/java/com/incetutku/restaurantlisting/repository/RestaurantRepository.java
@@ -0,0 +1,9 @@
+package com.incetutku.restaurantlisting.repository;
+
+import com.incetutku.restaurantlisting.entity.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepository extends JpaRepository<Restaurant, Integer> {
+}
