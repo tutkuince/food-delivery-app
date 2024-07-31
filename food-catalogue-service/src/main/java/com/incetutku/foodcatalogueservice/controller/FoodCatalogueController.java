ee686234bb14b35438143150e4748492ef559e6c
TUTKU\tutku <tutku.ince@outlook.com>
2024-07-31 22:47:28 +0300

    getRestaurantDetailsWithFoodMenu

--- food-catalogue-service/src/main/java/com/incetutku/foodcatalogueservice/controller/FoodCatalogueController.java
+++ food-catalogue-service/src/main/java/com/incetutku/foodcatalogueservice/controller/FoodCatalogueController.java
@@ -1,13 +1,11 @@
 package com.incetutku.foodcatalogueservice.controller;
 
+import com.incetutku.foodcatalogueservice.dto.FoodCataloguePage;
 import com.incetutku.foodcatalogueservice.dto.FoodItemDTO;
 import com.incetutku.foodcatalogueservice.service.FoodCatalogueService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/foodCatalogues")
@@ -23,5 +21,11 @@ public class FoodCatalogueController {
     public ResponseEntity<FoodItemDTO> createFoodItem(@RequestBody FoodItemDTO foodItemDTO) {
         FoodItemDTO savedFoodItem = foodCatalogueService.createFoodItem(foodItemDTO);
         return new ResponseEntity<>(savedFoodItem, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/getRestaurantAndFoodItemsById/{restaurantId}")
+    public ResponseEntity<FoodCataloguePage> getRestaurantDetailsWithFoodMenu(@PathVariable Integer restaurantId) {
+        FoodCataloguePage foodCataloguePage = foodCatalogueService.getFoodCataloguePageDetails(restaurantId);
+        return ResponseEntity.ok(foodCataloguePage);
     }
 }
