af10ee99c94bbad69822651bfb9ff0d8887a5d40
TUTKU\tutku <tutku.ince@outlook.com>
2024-07-31 22:25:43 +0300

    create Food Item service

--- food-catalogue-service/src/main/java/com/incetutku/foodcatalogueservice/service/FoodCatalogueService.java
+++ food-catalogue-service/src/main/java/com/incetutku/foodcatalogueservice/service/FoodCatalogueService.java
@@ -1,5 +1,8 @@
 package com.incetutku.foodcatalogueservice.service;
 
+import com.incetutku.foodcatalogueservice.dto.FoodItemDTO;
+import com.incetutku.foodcatalogueservice.entity.FoodItem;
+import com.incetutku.foodcatalogueservice.mapper.FoodItemMapper;
 import com.incetutku.foodcatalogueservice.repository.FoodItemRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +12,10 @@ public class FoodCatalogueService {
 
     public FoodCatalogueService(FoodItemRepository foodItemRepository) {
         this.foodItemRepository = foodItemRepository;
+    }
+
+    public FoodItemDTO createFoodItem(FoodItemDTO foodItemDTO) {
+        FoodItem savedFoodItem = foodItemRepository.save(FoodItemMapper.INSTANCE.mapFoodItemDTOToFoodItem(foodItemDTO));
+        return FoodItemMapper.INSTANCE.mapFoodItemToFoodItemDTO(savedFoodItem);
     }
 }
