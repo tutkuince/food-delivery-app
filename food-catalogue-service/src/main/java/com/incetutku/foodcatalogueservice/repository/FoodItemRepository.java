c41dd325632bbdeceef59ca5a475383e10f61f0a
TUTKU\tutku <tutku.ince@outlook.com>
2024-08-01 17:29:16 +0300

    new method

--- food-catalogue-service/src/main/java/com/incetutku/foodcatalogueservice/repository/FoodItemRepository.java
+++ food-catalogue-service/src/main/java/com/incetutku/foodcatalogueservice/repository/FoodItemRepository.java
@@ -4,6 +4,9 @@ import com.incetutku.foodcatalogueservice.entity.FoodItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FoodItemRepository extends JpaRepository<FoodItem, Integer> {
+    List<FoodItem> findFoodItemsByRestaurantId(int restaurantId);
 }
