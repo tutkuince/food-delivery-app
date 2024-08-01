2f13fc2a9a305c46d137625c3f5db37cf6592203
TUTKU\tutku <tutku.ince@outlook.com>
2024-08-01 22:11:47 +0300

    mapper

---
+++ order-service/src/main/java/com/incetutku/orderservice/mapper/OrderMapper.java
@@ -0,0 +1,14 @@
+package com.incetutku.orderservice.mapper;
+
+import com.incetutku.orderservice.dto.OrderDTO;
+import com.incetutku.orderservice.entity.Order;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface OrderMapper {
+    OrderMapper INSTANCE = Mappers.getMapper(OrderMapper.class);
+
+    Order mapOrderDTOToOrder(OrderDTO orderDTO);
+    OrderDTO mapOrderToOrderDTO(Order order);
+}
