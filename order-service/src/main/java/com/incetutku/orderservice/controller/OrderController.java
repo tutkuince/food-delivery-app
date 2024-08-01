8c2fd7781f4cd7cfa6502b5be67432e1102bd3b7
TUTKU\tutku <tutku.ince@outlook.com>
2024-08-01 22:10:29 +0300

    controller

---
+++ order-service/src/main/java/com/incetutku/orderservice/controller/OrderController.java
@@ -0,0 +1,28 @@
+package com.incetutku.orderservice.controller;
+
+import com.incetutku.orderservice.dto.CreateOrderDTO;
+import com.incetutku.orderservice.dto.OrderDTO;
+import com.incetutku.orderservice.service.OrderService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderDTO> createOrder(@RequestBody CreateOrderDTO createOrder) {
+        OrderDTO orderDTO = orderService.saveOrder(createOrder);
+        return new ResponseEntity<>(orderDTO, HttpStatus.CREATED);
+    }
+}
