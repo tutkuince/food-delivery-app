272b758d7d46a76f1e818200f6de714d068dc8fd
TUTKU\tutku <tutku.ince@outlook.com>
2024-08-02 19:26:43 +0300

    moved to components folder

--- app/src/app/header/header.module.ts
+++ app/src/app/header/header.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeaderRoutingModule } from './header-routing.module';
-import { HeaderComponent } from './header.component';
+import { HeaderComponent } from './components/header.component';
 
 
 @NgModule({
