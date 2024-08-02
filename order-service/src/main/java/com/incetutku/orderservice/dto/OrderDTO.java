850ce48a36e33ad55e26ff2d8940d6ff2bf48a72
TUTKU\tutku <tutku.ince@outlook.com>
2024-08-02 16:45:28 +0300

    fixed bugs - added missing annotations

--- order-service/src/main/java/com/incetutku/orderservice/dto/OrderDTO.java
+++ order-service/src/main/java/com/incetutku/orderservice/dto/OrderDTO.java
@@ -1,7 +1,14 @@
 package com.incetutku.orderservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderDTO {
     private String orderId;
     private RestaurantDTO restaurant;
