bf7ae95c76e03246146fce0e8e7a73ee333e3f04
TUTKU\tutku <tutku.ince@outlook.com>
2024-08-04 23:44:09 +0300

    updated for cross origin

--- order-service/src/main/java/com/incetutku/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/incetutku/orderservice/controller/OrderController.java
@@ -5,11 +5,9 @@ import com.incetutku.orderservice.dto.OrderDTO;
 import com.incetutku.orderservice.service.OrderService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
