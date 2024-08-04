775a71e0b0d00c04b46116155df39659795ef44e
TUTKU\tutku <tutku.ince@outlook.com>
2024-08-04 20:16:20 +0300

    restaurant-listing create module

---
+++ app/src/app/restaurant-listing/restaurant-listing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RestaurantListingRoutingModule } from './restaurant-listing-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RestaurantListingRoutingModule
+  ]
+})
+export class RestaurantListingModule { }
