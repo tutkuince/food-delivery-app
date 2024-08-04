b96ef132d2d51cc6dd188b778961a056d5ca1e97
TUTKU\tutku <tutku.ince@outlook.com>
2024-08-04 20:49:05 +0300

    routing

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {HeaderModule} from "./header/header.module";
+import {RestaurantListingModule} from "./restaurant-listing/restaurant-listing.module";
 
 @NgModule({
   declarations: [
@@ -12,9 +13,11 @@ import {HeaderModule} from "./header/header.module";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HeaderModule
+    HeaderModule,
+    RestaurantListingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- app/src/app/restaurant-listing/restaurant-listing-routing.module.ts
+++ app/src/app/restaurant-listing/restaurant-listing-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {RestaurantListingComponent} from "./components/restaurant-listing.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: RestaurantListingComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class RestaurantListingRoutingModule { }
+export class RestaurantListingRoutingModule {
+}
